714012d1a30dd576fe6bd1c40b3442d9396c0397
jarowlod <jarowlod@gmail.com>
2023-01-12 08:45:43 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,43 +1,42 @@
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const docDefinition = {
-    content: [
-      {
-        table: {
-          body: [
-            ["Column 1", "Column 2", "Column 3"],
-            ["One value goes here", "Another one here", "OK?"]
-          ],
-          layout: 'lightHorizontalLines',
-          headerRows: 1,
-          widths: [ 287.2, 100, 100 ],
-        }
+  pageSize: 'A4',
+  pageOrientation: 'landscape',
+  // pageOrientation: 'portreit',
+  pageMargins: [40, 70, 40, 60],
+  content: [
+    {
+      table: {
+        body: [
+          ['Column 1', 'Column 2', 'Column 3'],
+          ['One value goes here', 'Another one here', 'OK?'],
+        ],
+        headerRows: 1,
+        widths: [534, 100, 100],
       },
-      {
-        table: {
-          body: [
-            ["Column 1", "Column 2", "Column 3"],
-            ["One value goes here", "Another one here", "OK?"]
-          ],
-          layout: 'lightHorizontalLines',
-          headerRows: 1,
-          widths: [ '*', 100, 100 ],
-        }
-      }
-    ],
-  };
+    },
+    {
+      table: {
+        body: [
+          ['Column 1', 'Column 2', 'Column 3'],
+          ['One value goes here', 'Another one here', 'OK?'],
+        ],
+        headerRows: 1,
+        widths: ['*', 100, 100],
+      },
+    },
+  ],
+};
 
-
-    const pdfDocGenerator = pdfMake.createPdf(docDefinition);
-    pdfDocGenerator.getDataUrl((dataUrl) => {
-      const targetElement =  document.getElementById('app');
-      const iframe = document.createElement('iframe');
-      iframe.width = "100%"
-      iframe.height = "1000"
-      iframe.src = dataUrl;
-      targetElement.appendChild(iframe);
-    });
-
-
+const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+pdfDocGenerator.getDataUrl((dataUrl) => {
+  const targetElement = document.getElementById('app');
+  const iframe = document.createElement('iframe');
+  iframe.width = '100%';
+  iframe.height = '1000';
+  iframe.src = dataUrl;
+  targetElement.appendChild(iframe);
+});
