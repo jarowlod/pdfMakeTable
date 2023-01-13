6a7bb626f21b7cf9027635f9153f22cbb3513c6b
jarowlod <jarowlod@gmail.com>
2023-01-13 12:43:41 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
+const ff = 8;
+const gap= ff*0.172;
+
 const docDefinition = {
   pageSize: 'A4',
   pageOrientation: 'landscape',
@@ -9,6 +12,62 @@ const docDefinition = {
   pageMargins: [40, 70, 40, 60],
   content: [
     {
+      fontSize: ff,
+      table: {
+				body: [
+					[{
+            fontSize: ff,
+            table: {
+              body: [
+                ['row 1', 'column B'],
+                ['row 1\n row 2', 'column B'],
+                ['row 1\n row 2\n row 3', 'column B'],
+                ['row 7', 'column B'],
+                ['row 8', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 1', 'column B'],
+                ['row 27', 'column B'],
+                ['row 27', 'column B'],
+                ['row 27', 'column B'],
+                ['row 27', 'column B'],
+                ['row 27', 'column B'],
+                ['row 32', 'column B']
+              ]
+            }
+          },
+          {
+            fontSize: ff,
+            table: {
+              heights: [(ff+gap)*1,(ff+gap)*2,(ff+gap)*3],
+              body: [
+                ['row 1', 'column B'],
+                ['row 2', 'column B'],
+                ['row 3', 'column B']
+              ]
+            }
+          }]
+				]
+			}
+    },
+    {text: 'Column/row spans', pageBreak: 'before'},
+    {
+      fontSize: 8,
       table: {
         body: [
           ['Column 1', 'Column 2', 'Column 3'],
@@ -19,6 +78,7 @@ const docDefinition = {
       },
     },
     {
+      fontSize: 8,
       table: {
         body: [
           ['Column 1', 'Column 2', 'Column 3'],
@@ -27,9 +87,11 @@ const docDefinition = {
         headerRows: 1,
         widths: ['*', 100, 100],
       },
-    },
+    }
   ],
 };
+
+console.log('fontSize: ', ff, (ff+gap), heightOfString(), ff * 2.74);
 
 const pdfDocGenerator = pdfMake.createPdf(docDefinition);
 pdfDocGenerator.getDataUrl((dataUrl) => {
@@ -40,3 +102,30 @@ pdfDocGenerator.getDataUrl((dataUrl) => {
   iframe.src = dataUrl;
   targetElement.appendChild(iframe);
 });
+
+function heightOfString(text: string = 'QWERTY12', option = { width : 400, fontSize: 8 }): number {
+  const widthPx = Math.round(option.width * 2.74);
+  const fontSizePx = Math.round(option.fontSize * 2.74);
+  const span = getDocumentElementOfTextBlockOverflowWrap(widthPx, fontSizePx);
+
+  span.innerText = text;
+  const rowHeight = span.offsetHeight;
+  span.remove();
+
+  return rowHeight - 1;
+}
+
+function getDocumentElementOfTextBlockOverflowWrap(columnWidthPx: number, fontSizePx: number): HTMLSpanElement {
+  const span = document.createElement('span');
+  span.style.position = 'fixed';
+  span.style.visibility = 'hidden';
+  span.style.fontFamily = 'Ubuntu';
+  span.style.overflowWrap = 'break-word';
+  span.style.padding = '6px 6px 6px 8px';
+  span.style.border = 'solid 2px';
+  span.style.lineHeight = 'normal';
+  span.style.width = `${ columnWidthPx }px`;
+  span.style.fontSize = `${ fontSizePx }px`;
+  document.body.appendChild(span);
+  return span;
+}
