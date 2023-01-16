cd66eb938142bd66a7d568d8ada2e5d9aec20ea6
jarowlod <jarowlod@gmail.com>
2023-01-16 23:48:52 +0100

    podwójne renderowanie w celu wyrycia indexów na stronę

--- index.ts
+++ index.ts
@@ -2,108 +2,97 @@ import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const ff = 8;
-const gap= ff*0.172;
+const ff = 12;
+const gap = ff * 0.172;
+
+const rows = [
+  ['title A\n tyyt', 'title B'],
+  ['sum 1', ''],
+  ['sum 2', ''],
+];
+for (let i = 1; i <= 80; i++) {
+  rows.push([
+    { text: `row ${i}`, id: i },
+    { text: `row sdf dgfgdfgdfg dfgdfdfgfghfghfghfghdfggd ${i}` },
+  ]);
+}
+const pagesIndex = [];
 
 const docDefinition = {
   pageSize: 'A4',
   pageOrientation: 'landscape',
   // pageOrientation: 'portreit',
   pageMargins: [40, 70, 40, 60],
+  pageBreakBefore: function (
+    currentNode,
+    followingNodesOnPage,
+    nodesOnNextPage,
+    previousNodesOnPage
+  ) {
+    if (currentNode.id) {
+      console.log(currentNode.id, currentNode.pageNumbers);
+      const page = currentNode.pageNumbers[0] - 1;
+      pagesIndex[page] = currentNode.id;
+    }
+    return false;
+  },
   content: [
     {
       fontSize: ff,
       table: {
-				body: [
-					[{
-            fontSize: ff,
-            table: {
-              body: [
-                ['row 1', 'column B'],
-                ['row 1\n row 2', 'column B'],
-                ['row 1\n row 2\n row 3', 'column B'],
-                ['row 7', 'column B'],
-                ['row 8', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 1', 'column B'],
-                ['row 27', 'column B'],
-                ['row 27', 'column B'],
-                ['row 27', 'column B'],
-                ['row 27', 'column B'],
-                ['row 27', 'column B'],
-                ['row 32', 'column B']
-              ]
-            }
-          },
-          {
-            fontSize: ff,
-            table: {
-              heights: [(ff+gap)*1,(ff+gap)*2,(ff+gap)*3],
-              body: [
-                ['row 1', 'column B'],
-                ['row 2', 'column B'],
-                ['row 3', 'column B']
-              ]
-            }
-          }]
-				]
-			}
-    },
-    {text: 'Column/row spans', pageBreak: 'before'},
-    {
-      fontSize: 8,
-      table: {
-        body: [
-          ['Column 1', 'Column 2', 'Column 3'],
-          ['One value goes here', 'Another one here', 'OK?'],
-        ],
-        headerRows: 1,
-        widths: [534, 100, 100],
+        headerRows: 3,
+        dontBreakRows: true,
+        widths: [50, 100],
+        body: rows,
       },
     },
-    {
-      fontSize: 8,
-      table: {
-        body: [
-          ['Column 1', 'Column 2', 'Column 3'],
-          ['One value goes here', 'Another one here', 'OK?'],
-        ],
-        headerRows: 1,
-        widths: ['*', 100, 100],
-      },
-    }
   ],
 };
 
-console.log('fontSize: ', ff, (ff+gap), heightOfString(), ff * 2.74);
+console.log('fontSize: ', ff, ff + gap, heightOfString(), ff * 2.74);
 
 const pdfDocGenerator = pdfMake.createPdf(docDefinition);
-pdfDocGenerator.getDataUrl((dataUrl) => {
-  const targetElement = document.getElementById('app');
-  const iframe = document.createElement('iframe');
-  iframe.width = '100%';
-  iframe.height = '1000';
-  iframe.src = dataUrl;
-  targetElement.appendChild(iframe);
+pdfDocGenerator.getBuffer((dataUrl) => {
+  // const targetElement = document.getElementById('app');
+  // const iframe = document.createElement('iframe');
+  // iframe.width = '100%';
+  // iframe.height = '1000';
+  // iframe.src = dataUrl;
+  // targetElement.appendChild(iframe);
+
+  console.log(pagesIndex);
+  rows.splice(1, 2);
+  const sums = [];
+  pagesIndex.forEach((v, i) => {
+    const s = rows
+      .slice((pagesIndex[i - 1] ?? 0) + 2 * i + 1, v +  2 * i + 1)
+      .shift()[0]['id'];
+    const e = rows.slice((pagesIndex[i - 1] ?? 0) + 2 * i + 1, v + 2 * i + 1).pop()[0]['id'];
+    sums[i] = s + ' : ' + e;
+    rows.splice(
+      pagesIndex[i] + 2 * i + 1,
+      0,
+      ['sum 1', sums[i]],
+      ['sum 2', '']
+    );
+  });
+
+  docDefinition.content[0].table.headerRows = 1;
+  const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+  pdfDocGenerator.getDataUrl((dataUrl) => {
+    const targetElement = document.getElementById('app');
+    const iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = '1000';
+    iframe.src = dataUrl;
+    targetElement.appendChild(iframe);
+  });
 });
 
-function heightOfString(text: string = 'QWERTY12', option = { width : 400, fontSize: 8 }): number {
+function heightOfString(
+  text: string = 'QWERTY12',
+  option = { width: 400, fontSize: 8 }
+): number {
   const widthPx = Math.round(option.width * 2.74);
   const fontSizePx = Math.round(option.fontSize * 2.74);
   const span = getDocumentElementOfTextBlockOverflowWrap(widthPx, fontSizePx);
@@ -115,7 +104,10 @@ function heightOfString(text: string = 'QWERTY12', option = { width : 400, fontS
   return rowHeight - 1;
 }
 
-function getDocumentElementOfTextBlockOverflowWrap(columnWidthPx: number, fontSizePx: number): HTMLSpanElement {
+function getDocumentElementOfTextBlockOverflowWrap(
+  columnWidthPx: number,
+  fontSizePx: number
+): HTMLSpanElement {
   const span = document.createElement('span');
   span.style.position = 'fixed';
   span.style.visibility = 'hidden';
@@ -124,8 +116,8 @@ function getDocumentElementOfTextBlockOverflowWrap(columnWidthPx: number, fontSi
   span.style.padding = '6px 6px 6px 8px';
   span.style.border = 'solid 2px';
   span.style.lineHeight = 'normal';
-  span.style.width = `${ columnWidthPx }px`;
-  span.style.fontSize = `${ fontSizePx }px`;
+  span.style.width = `${columnWidthPx}px`;
+  span.style.fontSize = `${fontSizePx}px`;
   document.body.appendChild(span);
   return span;
 }
